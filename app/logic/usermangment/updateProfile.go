ce927b73165e43e57d677cb1ba22d079354a8313
tsaadsal <tounsadisaadsalah@gmail.com>
2025-01-03 17:40:36 +0100

    update updating profile in order that each one changes separetly

--- app/logic/usermangment/updateProfile.go
+++ app/logic/usermangment/updateProfile.go
@@ -6,6 +6,7 @@ import (
 	"errors"
 	"io"
 	"net/http"
+	"time"
 
 	"golang.org/x/crypto/bcrypt"
 )
@@ -25,23 +26,73 @@ func UpdateUserProfile(dataReader io.ReadCloser, req *http.Request) error {
 	if err != nil || cookie.Value == "" {
 		return errors.New("no cookie")
 	}
-	if err := newUserData.UpdateInfos(db, cookie.Value); err != nil {
+
+	if err := newUserData.CheckInfos(db); err != nil {
 		return err
 	}
+
 	return nil
 }
 
 func (User *User) UpdateInfos(db *sql.DB, cookieValue string) error {
-	hashedPassWord, err := bcrypt.GenerateFromPassword([]byte(User.Password), 12)
-	if err != nil {
-		return errors.New("internal server error")
+	if User.Username != "" {
+		_, err := db.Exec("UPDATE users SET username=?,updated_at=? WHERE uuid=? ", User.Username, time.Now().Format(time.DateTime), cookieValue)
+		if err != nil {
+			return errors.New("internal server error")
+		}
 	}
-	res, err := db.Exec("UPDATE users WHERE uuid=? SET username=? email=? password=? VALUES(?, ?, ?, ?)", cookieValue, User.Username, User.Email, hashedPassWord)
-	if err != nil {
-		return errors.New("cannot change infos")
+	if User.Email != "" {
+		_, err := db.Exec("UPDATE users SET email=?, updated_at=? WHERE uuid=? ", User.Email, time.Now().Format(time.DateTime), cookieValue)
+		if err != nil {
+			return errors.New("internal server error")
+		}
 	}
-	if afected, err := res.RowsAffected(); err != nil || afected == 0 {
-		return errors.New("account doesn't exist")
+	if User.Password != "" {
+		hashedPassWord, err := bcrypt.GenerateFromPassword([]byte(User.Password), 12)
+		if err != nil {
+			return errors.New("internal server error")
+		}
+		_, err = db.Exec("UPDATE users SET password=?,updated_at=? WHERE uuid=? ", hashedPassWord, time.Now().Format(time.DateTime), cookieValue)
+		if err != nil {
+			return errors.New("internal server error")
+		}
 	}
+
+	return nil
+}
+
+func (User *User) CheckInfos(db *sql.DB) error {
+	var err error
+	if User.Username != "" {
+		err = ValidUserName(User.Username)
+		if err != nil {
+			return err
+		}
+		err = db.QueryRow("SELECT (*) FROM users WHERE username=?", User.Username).Scan()
+		if err == nil {
+			return errors.New("username taken")
+		} else if err != sql.ErrNoRows {
+			return errors.New("internal server error")
+		}
+	}
+	if User.Email != "" {
+		err = ValidEmail(User.Email)
+		if err != nil {
+			return err
+		}
+		err = db.QueryRow("SELECT (*) FROM users WHERE email=?", User.Email).Scan()
+		if err == nil {
+			return errors.New("email already exists")
+		} else if err != sql.ErrNoRows {
+			return errors.New("internal server error")
+		}
+	}
+	if User.Password != "" {
+		err = ValidPassword(User.Password, User.PasswordConfirm)
+		if err != nil {
+			return err
+		}
+	}
+
 	return nil
 }
